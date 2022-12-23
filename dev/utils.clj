6bd19ab83ca5ae21e6e4887c78f7d327ade2f6d7
Dmitry Egorov <9136211+ddegr@users.noreply.github.com>
2022-12-23 15:05:58 +0200

    Remove git push on release
    Co-authored-by: @apostaat <alexeevdev@yahoo.com>

--- dev/utils.clj
+++ dev/utils.clj
@@ -31,7 +31,6 @@
     (shell/sh "sh" "-c" "git add version.edn pom.xml")
     (shell/sh "sh" "-c" (format "git commit -m 'Bump version to %s'" new-version-str))
     (shell/sh "sh" "-c" (str "git tag " new-version-str))
-    (shell/sh "sh" "-c" "git push --tags")
     (version)))
 
 (defn bump-major
