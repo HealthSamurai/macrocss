5a2a499947b056d7106b211330fc2e9f8ff5eeb7
Artem Apostatov <alexeevdev@yahoo.com>
2022-12-23 17:23:36 +0600

    bump version utils and add tags
    Co-authored-by: @ddegr <de@health-samurai.io>
    Co-authored-by: @vganshin <ganshinv@gmail.com>

---
+++ dev/utils.clj
@@ -0,0 +1,52 @@
+(ns utils
+  (:require [clojure.edn :as edn]
+            [clojure.string :as str]
+            [clojure.java.shell :as shell]))
+
+(defn version [] (edn/read-string (slurp "version.edn")))
+
+(defn version->str
+  [ver]
+  (->> (vals ver)
+       (str/join ".")))
+
+(defn save-version
+  [new-ver]
+  (spit "version.edn" new-ver))
+
+(defn update-pom-xml
+  [new-ver]
+  (let [new-version-xml (str "<version>" new-ver "</version>")
+        new-pom-xml (str/replace-first
+                     (slurp "pom.xml")
+                     (re-pattern "<version>.+</version>")
+                     new-version-xml)]
+    (spit "pom.xml" new-pom-xml)))
+
+(defn propagate-updates
+  [new-version]
+  (let [new-version-str (version->str new-version)]
+    (utils/save-version new-version)
+    (utils/update-pom-xml new-version-str)
+    (shell/sh "sh" "-c" (str "git tag " new-version-str))
+    (version)))
+
+(defn bump-major
+  []
+  (-> (version)
+      (update :major inc)
+      (merge {:minor 0 :fix 0})
+      propagate-updates))
+
+(defn bump-minor
+  []
+  (-> (version)
+      (update :minor inc)
+      (merge {:fix 0})
+      propagate-updates))
+
+(defn bump-fix
+  []
+  (-> (version)
+      (update :fix inc)
+      propagate-updates))
